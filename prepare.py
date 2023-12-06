4e6d0837e3abb343793dfc06a46e536c23cec334
Ricardo Benitez <rickybenz35@gmail.com>
2023-12-05 23:24:12 -0600

    updated

--- prepare.py
+++ prepare.py
@@ -35,32 +35,36 @@ def splitting_data(df, col):
     return train, validate, test
 
 
-    def telco_encoded(train, validate, test):
-         """
-         One-hot (For each unique category in a categorical variable, a new binary column (dummy variable) is created.) encodes categorical columns in the input DataFrames (train, validate, and test),
-    excluding specific columns ('customer_id' and 'total_charges') from the encoding process.
-
-    Parameters:
-    - train (pd.DataFrame): Training dataset.
-    - validate (pd.DataFrame): Validation dataset.
-    - test (pd.DataFrame): Test dataset.
-
-    Returns:
-    List of pd.DataFrames:
-    - train_encoded: Training dataset with one-hot encoded categorical columns.
-    - validate_encoded: Validation dataset with one-hot encoded categorical columns.
-    - test_encoded: Test dataset with one-hot encoded categorical columns.
-    """
-         encoded_dfs = []
-    for df in [train, validate, test]:
-        df_encoded = df.copy()
-        for col in df.columns:
-            if col == 'customer_id':
-                continue
-            if col == 'total_charges':
-                continue
-            elif df[col].dtype == 'O':  
-                df_dummies = pd.get_dummies(df[col], prefix=col, drop_first=True).astype(int)
-                df_encoded = df_encoded.join(df_dummies).drop(columns=[col])
-        encoded_dfs.append(df_encoded)
+    def preprocess_telco(train_df, val_df, test_df):
+    '''
+    Sets the index of each DataFrame to 'customer_id'.
+    Converts the 'total_charges' column to the float data type.
+    Identifies categorical variables for one-hot encoding.
+    One-hot encodes the identified categorical variables with drop_first=True.
+    Concatenates the original DataFrame with the one-hot encoded variables.
+    Drops the original categorical variables from the DataFrame
+    '''
+    # using a for loop:
+    # go through the three dfs, set the index to customer id
+    for df in [train_df, val_df, test_df]:
+        df = df.set_index('customer_id')
+        df['total_charges'] = df['total_charges'].astype(float)
+    # initialize an empty list to see what needs to be encoded:
+    encoding_vars = []
+    # looping through the columns to fill encoded_vars with appropriate
+    # datatype field names
+    for col in train_df.columns:
+        if train_df[col].dtype == 'O':
+            encoding_vars.append(col)
+    encoding_vars.remove('customer_id')
+    # create an empty list so it can hold the encoded dataframes:
+    encoded_dfs = []
+    for df in [train_df, val_df, test_df]:
+        df_encoded_cats = pd.get_dummies(
+            df[encoding_vars],
+              drop_first=True).astype(int)
+        encoded_dfs.append(pd.concat(
+            [df,
+            df_encoded_cats],
+            axis=1).drop(columns=encoding_vars))
     return encoded_dfs
